e6a76a4402b0a1343c729175897b22ba4a0f7bb4
niki <nukutossukk@gmail.com>
2024-12-10 10:20:37 +0300

    add files

---
+++ .code/detect/kagle.py
@@ -0,0 +1,25 @@
+import kagglehub
+
+# Download latest version
+#path = kagglehub.dataset_download("kmader/drone-videos")
+
+#print("Path to dataset files:", path)
+
+# Download latest version
+#path = kagglehub.dataset_download("bulentsiyah/semantic-drone-dataset")
+
+#print("Path to dataset files:", path)
+
+#path = kagglehub.dataset_download("rgbnihal/c2a-dataset")
+
+#print("Path to dataset files:", path)
+
+#path = kagglehub.dataset_download("aryashah2k/stanford-drone-dataset")
+
+#print("Path to dataset files:", path)
+
+
+#from datasets import load_dataset
+
+#ds = load_dataset("nominal-io/drone-flight-object-identification")
+
